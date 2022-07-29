7678e64cd92508437f910c189209338fd7066aaa
Ravinder Kaushik <105774953+Ravinder2407@users.noreply.github.com>
2022-07-29 18:22:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -171,10 +171,10 @@ class EasyApplyLinkedin:
         self.close_session()
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
 
-#     with open('config.json') as config_file:
-#         data = json.load(config_file)
+    with open('config.json') as config_file:
+        data = json.load(config_file)
 
-#     bot = EasyApplyLinkedin(data)
-#     bot.apply()
+    bot = EasyApplyLinkedin(data)
+    bot.apply()
